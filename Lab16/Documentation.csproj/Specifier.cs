156b9bfadab0c045cb66903e771cb25aef664e4d
GrigoriyVoronin <grigoriy.1998@mail.ru>
2020-05-23 10:53:18 +0500

    16 lab solved

--- Lab16/Documentation.csproj/Specifier.cs
+++ Lab16/Documentation.csproj/Specifier.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using System.Reflection;
 
@@ -8,37 +7,116 @@ namespace Documentation
     {
         public string GetApiDescription()
         {
-            throw new NotImplementedException();
+            return (typeof(T)
+                    .GetCustomAttributes(typeof(ApiDescriptionAttribute))
+                    .FirstOrDefault() as ApiDescriptionAttribute)?
+                .Description;
         }
 
         public string[] GetApiMethodNames()
         {
-            
+            return typeof(T).GetMethods()
+                .Where(x
+                    => x.CustomAttributes
+                           .FirstOrDefault(y => y.AttributeType == typeof(ApiMethodAttribute))
+                       != null)
+                .Select(x => x.Name)
+                .ToArray();
         }
 
         public string GetApiMethodDescription(string methodName)
         {
-            throw new NotImplementedException();
+            return (typeof(T)
+                    .GetMethod(methodName)?
+                    .GetCustomAttribute(typeof(ApiDescriptionAttribute)) as ApiDescriptionAttribute)?
+                .Description;
         }
 
         public string[] GetApiMethodParamNames(string methodName)
         {
-            throw new NotImplementedException();
+            return typeof(T)
+                .GetMethod(methodName)?
+                .GetParameters()
+                .Select(x => x.Name)
+                .ToArray();
         }
 
         public string GetApiMethodParamDescription(string methodName, string paramName)
         {
-            throw new NotImplementedException();
+            return (typeof(T)
+                    .GetMethod(methodName)?
+                    .GetParameters()
+                    .FirstOrDefault(x => x.Name == paramName)?
+                    .GetCustomAttribute(typeof(ApiDescriptionAttribute)) as ApiDescriptionAttribute)?
+                .Description;
         }
 
         public ApiParamDescription GetApiMethodParamFullDescription(string methodName, string paramName)
         {
-            throw new NotImplementedException();
+            var parametr = typeof(T)
+                .GetMethod(methodName)?
+                .GetParameters()
+                .FirstOrDefault(x => x.Name == paramName);
+            return parametr == null
+                ? new ApiParamDescription {ParamDescription = new CommonDescription(paramName)}
+                : CreateParamDescription(parametr, methodName, paramName);
+        }
+
+        private ApiParamDescription CreateParamDescription(ParameterInfo parametr, string methodName, string paramName)
+        {
+            var atrIntValidation = parametr
+                .GetCustomAttribute(typeof(ApiIntValidationAttribute)) as ApiIntValidationAttribute;
+            var atrRequired = parametr
+                .GetCustomAttribute(typeof(ApiRequiredAttribute)) as ApiRequiredAttribute;
+            var parametrDescription = new ApiParamDescription
+            {
+                MaxValue = atrIntValidation?.MaxValue,
+                MinValue = atrIntValidation?.MinValue,
+                ParamDescription = new CommonDescription
+                {
+                    Description = GetApiMethodParamDescription(methodName, paramName),
+                    Name = paramName
+                }
+            };
+            if (atrRequired != null)
+                parametrDescription.Required = atrRequired.Required;
+
+            return parametrDescription;
         }
 
         public ApiMethodDescription GetApiMethodFullDescription(string methodName)
         {
-            throw new NotImplementedException();
+            return typeof(T)
+                .GetMethod(methodName)?
+                .GetCustomAttribute(typeof(ApiMethodAttribute)) == null
+                ? null
+                : new ApiMethodDescription
+                {
+                    ParamDescriptions = GetApiMethodParamNames(methodName)
+                        .Select(x => GetApiMethodParamFullDescription(methodName, x))
+                        .ToArray(),
+                    MethodDescription = new CommonDescription
+                    {
+                        Description = GetApiMethodDescription(methodName),
+                        Name = methodName
+                    },
+                    ReturnDescription = GetApiMethodReturnParam(methodName)
+                };
+        }
+
+        private ApiParamDescription GetApiMethodReturnParam(string methodName)
+        {
+            var returnParametr = typeof(T)
+                .GetMethod(methodName)?
+                .ReturnParameter;
+            if (returnParametr?.Name == null)
+                return null;
+
+            var returnDescription = CreateParamDescription(returnParametr, methodName, returnParametr.Name);
+            returnDescription.ParamDescription.Name = returnDescription.ParamDescription.Name == ""
+                ? null
+                : returnDescription.ParamDescription.Name;
+            return returnDescription;
         }
     }
 }
